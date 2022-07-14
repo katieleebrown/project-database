b5ae3d8f26cfd9c1be197d3279fd2d67b29405c7
Katie Brown <katieleebrown19@gmail.com>
2022-07-14 12:54:20 -0400

    base files created, ejs for database cards in progress

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'projects',
+    collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('Connected to Database')
+        db = client.db(dbName)
+        collection = db.collection('projects')
+    })
+
+// Middleware comes before CRUD operations
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (request, response) => {
+    db.collection('characters-and-players').find().toArray()
+        .then(data => {
+            response.render('index.ejs', { info: data })
+        })
+})
+
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running`)
+})
